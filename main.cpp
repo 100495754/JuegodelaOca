c923771580c2964d504359f88fa4f6cfab388e09
100495754 <100495754@alumnos.uc3m.es>
2024-09-01 13:13:39 +0200

    juego funcional, falta efectoTiradas.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <ctime>
 
+const bool DEBUG = true;
 const int NUM_CASILLAS = 63;
 const int PUENTE_1 = 6;
 const int PUENTE_2 = 12;
@@ -28,7 +29,6 @@ bool esOca(int casilla) {
         if (i % 2 ==0) {oca +=4;}
         else {oca +=5;}
         i++;
-        std::cout<<oca<<std::endl;
     }
     return false;
 }
@@ -113,24 +113,43 @@ int siguienteMuerte(int casilla) {
 int tirarDado() {
     return 1 + rand() % 6;//limiteInferior + rand() % (limiteSuperior+1-limiteInferior).
 }
+int tirarDadoManual(int casillaActual) {
+    int avance;
+    std::cout << "INTRODUCE EL VALOR DEL DADO: " << std::endl;
+    std::cin >> avance ;
+    std::cout << "VALOR DEL DADO: "<< avance << std::endl;
+    std::cout << "PASAS A LA CASILLA: "<< casillaActual + avance << std::endl;
+    return casillaActual + avance;
+
+}
 int quienEmpieza() {
     return 1 + rand() % 2;
 }
 int efectoPosicion(int casillaActual) {
     if (esOca(casillaActual)) {
-        casillaActual= siguientePuente(casillaActual);
+        std::cout << "HAS CAIDO EN UNA OCA" << std::endl;
+        casillaActual= siguienteOca(casillaActual);
+        std::cout << "SALTAS A LA SIGUIENTE OCA EN LA CASILLA: "<< casillaActual << std::endl;
     }
     else if (esPuente(casillaActual)) {
+        std::cout << "HAS CAIDO EN UN PUENTE" << std::endl;
         casillaActual= siguientePuente(casillaActual);
+        std::cout << "DE PUENTE EN PUENTE ACABO EN: "<< casillaActual << std::endl;
     }
     else if (esDados(casillaActual)) {
+        std::cout << "HAS CAIDO EN DADOS" << std::endl;
         casillaActual= siguienteDado(casillaActual);
+        std::cout << "DE DADO EN DADO ACABO EN: "<< casillaActual << std::endl;
     }
     else if (esLaberinto(casillaActual)) {
+        std::cout << "HAS CAIDO EN UN LABERINTO" << std::endl;
         casillaActual= siguienteLaberinto(casillaActual);
+        std::cout << "ME PIERDO EN UN LABERINTO Y RETROCEDO 12, AHORA ESTOY EN: "<< casillaActual << std::endl;
     }
     else if (esMuerte(casillaActual)) {
+        std::cout << "HAS CAIDO EN LA MUERTE" << std::endl;
         casillaActual= siguienteMuerte(casillaActual);
+        std::cout << "HE MUERTO Y ACABO EN: "<< casillaActual << std::endl;
     }
     return casillaActual;
 }
@@ -149,12 +168,67 @@ int efectoTiradas(int casillaActual, int numeroDeTiradas) {
 }
 
 
+int do_turno(int jugador) {
+    jugador = efectoPosicion(jugador);
+    efectoTiradas(jugador,1);
+    std::cout << "ESTAS EN: " << jugador <<std::endl;
+    std::cout << "" << std::endl;
+    return jugador;
+}
+
+void turno(int &jugador) {
+    std::cout << "CASILLA ACTUAL: " << jugador <<std::endl;
+    if (DEBUG) {
+        jugador = tirarDadoManual(jugador);
+        jugador = do_turno(jugador);
+    }else {
+        jugador += tirarDado();
+        jugador = do_turno(jugador);
+    }
+}
+
 int main() {
     srand(time(NULL));
-    if (quienEmpieza()==1) {
+    int jugador1 = 1, jugador2 = 1;
+    bool final = false;
+    int empieza = quienEmpieza();
+    if (empieza == 1) {
         std::cout<<"**** Empieza el jugador 1 ****"<<std::endl;
+        turno(jugador1);
+        std::cout<<"** TURNO DEL JUGADOR 2 **"<<std::endl;
+        turno(jugador2);
+
     }else {
         std::cout<<"**** Empieza el jugador 2 ****"<<std::endl;
+        turno(jugador2);
+        std::cout<<"** TURNO DEL JUGADOR 1 **"<<std::endl;
+        turno(jugador1);
     }
+
+    while (!final) {
+        if (esMeta(jugador1)) {
+            std::cout << "**** Jugador 1 ha ganado! ****" << std::endl;
+            final = true;
+            return 0;
+
+        } else if (esMeta(jugador2)) {
+            std::cout << "**** Jugador 2 ha ganado! ****" << std::endl;
+            final = true;
+            return 0;
+        }
+
+        if (empieza == 1) {
+            std::cout<<"** TURNO DEL JUGADOR 1 **"<<std::endl;
+            turno(jugador1);
+            std::cout<<"** TURNO DEL JUGADOR 2 **"<<std::endl;
+            turno(jugador2);
+        }else {
+            std::cout<<"** TURNO DEL JUGADOR 2 **"<<std::endl;
+            turno(jugador2);
+            std::cout<<"** TURNO DEL JUGADOR 1 **"<<std::endl;
+            turno(jugador1);
+        }
+    }
+
     return 0;
 }
